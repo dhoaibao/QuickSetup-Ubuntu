80a8200e27f383fb20b1b135d24be000bfad407c
Duong Hoai Bao <dhbao2505@gmail.com>
2024-07-24 17:50:15 +0700

    Update QuickSetup.sh

--- QuickSetup.sh
+++ QuickSetup.sh
@@ -1,8 +1,8 @@
 sudo apt-get update && sudo apt-get upgrade -y
 
 sudo rm -rf /var/cache/snapd/
-sudo apt autoremove --purge snapd gnome-software-plugin-snap
-rm -fr ~/snap
+sudo apt autoremove --purge snapd gnome-software-plugin-snap -y
+sudo rm -fr ~/snap
 sudo apt-mark hold snapd
 
 sudo apt-get install -y nala
