a2b02904435dfca5543a1bc7f5b944730cafcd88
Duong Hoai Bao <dhbao2505@gmail.com>
2024-07-30 12:34:06 +0700

    update

--- QuickSetup.sh
+++ QuickSetup.sh
@@ -65,6 +65,14 @@ git clone https://github.com/dhoaibao/activate-StarUML-license.git
 sudo mv activate-StarUML-license/app.asar /opt/StarUML/resources
 rm -rf activate-StarUML-license/
 
+# Install ibus-bamboo
+sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
+sudo apt-get update
+sudo apt-get install ibus ibus-bamboo --install-recommends
+ibus restart
+# Đặt ibus-bamboo làm bộ gõ mặc định
+env DCONF_PROFILE=ibus dconf write /desktop/ibus/general/preload-engines "['BambooUs', 'Bamboo']" && gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'Bamboo')]"
+
 # Remove snap and related packages
 sudo rm -rf /var/cache/snapd/
 sudo apt autoremove --purge snapd gnome-software-plugin-snap -y
