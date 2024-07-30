43f895a7a709223c815bef09c4bbe3ef4345a2d5
Duong Hoai Bao <dhbao2505@gmail.com>
2024-07-30 12:27:12 +0700

    update

--- QuickSetup.sh
+++ QuickSetup.sh
@@ -11,7 +11,7 @@ sudo nala fetch
 sudo nala upgrade -y
 
 # Install essential packages
-sudo nala install -y git zsh wget curl nodejs npm neovim ibus-unikey gnome-tweaks gnome-shell-extension-manager flatpak gnome-software-plugin-flatpak
+sudo nala install -y git zsh gh wget curl nodejs npm neovim ibus-unikey gnome-tweaks gnome-shell-extension-manager flatpak gnome-software-plugin-flatpak
 
 # Add Flathub repository
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
