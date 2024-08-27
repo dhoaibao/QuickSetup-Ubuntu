b222adc0cdb9bcecce0bdcdca766900c00f307c1
Duong Hoai Bao <dhbao2505@gmail.com>
2024-08-28 00:08:14 +0700

    update

--- QuickSetup-Next.sh
+++ QuickSetup-Next.sh
@@ -10,7 +10,6 @@ sudo mv ./cursors/* /usr/share/icons
 git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
 WhiteSur-gtk-theme/install.sh -l
 WhiteSur-gtk-theme/tweaks.sh -F
-WhiteSur-gtk-theme/tweaks.sh -f monterey
 sudo flatpak override --filesystem=xdg-config/gtk-4.0
 sudo WhiteSur-gtk-theme/tweaks.sh -g
 rm -rf WhiteSur-gtk-theme/
