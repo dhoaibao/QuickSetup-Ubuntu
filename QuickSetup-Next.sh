6b5642949fd0c749844d9246e8b81890782ae8fd
Duong Hoai Bao <dhbao2505@gmail.com>
2024-09-28 22:10:20 +0700

    Update QuickSetup-Next.sh

--- QuickSetup-Next.sh
+++ QuickSetup-Next.sh
@@ -8,9 +8,7 @@ sudo mv ./cursors/* /usr/share/icons
 
 # Install WhiteSur GTK
 git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
-WhiteSur-gtk-theme/install.sh -l
-WhiteSur-gtk-theme/tweaks.sh -F
-sudo flatpak override --filesystem=xdg-config/gtk-4.0
+WhiteSur-gtk-theme/install.sh
 sudo WhiteSur-gtk-theme/tweaks.sh -g -b "ubuntu-2.png"
 rm -rf WhiteSur-gtk-theme/
 
